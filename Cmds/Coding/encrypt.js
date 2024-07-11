4f4079a3dbd5949f37e29c27a4091f7b475681e3
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-11 20:13:32 +0300

    Add files via upload

---
+++ Cmds/Coding/encrypt.js
@@ -0,0 +1,27 @@
+module.exports = async (context) => {
+    const { m } = context;
+
+    const Obf = require("javascript-obfuscator");
+
+    // Check if the quoted message has text
+    if (m.quoted && m.quoted.text) {
+        const forq = m.quoted.text;
+
+        // Obfuscate the JavaScript code
+        const obfuscationResult = Obf.obfuscate(forq, {
+            compact: false,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 1,
+            numbersToExpressions: true,
+            simplify: true,
+            stringArrayShuffle: true,
+            splitStrings: true,
+            stringArrayThreshold: 1
+        });
+
+        console.log("Successfully encrypted the code");
+        m.reply(obfuscationResult.getObfuscatedCode());
+    } else {
+        m.reply("Tag a valid JavaScript code to encrypt!");
+    }
+};
