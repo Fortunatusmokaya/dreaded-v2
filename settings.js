60450da2333f7f81483623699741d9f119ab6074
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-12 13:54:13 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 You don't have to set this if you deploy using heroku because you can simply set them in environment variables, also don't forget to sleep */
 
 
-
+const session = process.env.SESSION || '';
 
 const prefix = process.env.PREFIX || '.';
 const mycode = process.env.CODE || "254";
@@ -36,5 +36,6 @@ module.exports = {
   dev,
   DevDreaded,
   gcpresence,
-  antionce
+  antionce,
+session
 };
