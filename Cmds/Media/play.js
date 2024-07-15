65f3c9d406d8e73d032588e722847ad0c913680b
MG TOPHAZ <130512949+MGTOPHAZ@users.noreply.github.com>
2024-07-15 22:19:37 +0300

    Update play.js

--- Cmds/Media/play.js
+++ Cmds/Media/play.js
@@ -1,11 +1,10 @@
- const yts= require("yt-search")
-
-const YT = require("../../lib/musicdl.js");
+ const yts = require("yt-search");
+const ytdl = require("@distube/ytdl-core");
 const fs = require("fs");
 
 
 module.exports = async (context) => {
-    const { client, m, text, fetchBuffer } = context;     
+    const { client, m, text } = context;     
 
 
 if (!text) {
@@ -13,28 +12,52 @@ if (!text) {
             return;
         }
         try {
-           
-        let search = await yts(text)
-        let anu = search.videos[0]
-const pl= await YT.mp4(anu.url)
+            const {
+                videos
+            } = await yts(text);
+            if (!videos || videos.length <= 0) {
+                reply(`No songs found.`)
+                return;
+            }
+            let urlYt = videos[0].url
+            let infoYt = await ytdl.getInfo(urlYt);
 
-await client.sendMessage(m.chat,{
-    document: {url:pl.videoUrl},
-    fileName: anu.title + '.mp4',
-    mimetype: 'video/mp4',
-    contextInfo:{
-        externalAdReply:{
-            title:anu.title,
-            body: "DREADED V2",
-            thumbnail: await fetchBuffer(anu.thumbnail),
-            mediaType:2,
-            mediaUrl:anu.url,
+
+            const getRandonm = (ext) => {
+                return `${Math.floor(Math.random() * 10000)}${ext}`;
+            };
+            let titleYt = infoYt.videoDetails.title;
+            let randomName = getRandonm(".mp3");
+            const stream = ytdl(urlYt, {
+                    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
+                })
+                .pipe(fs.createWriteStream(`./${randomName}`));
+            console.log("Audio downloading ->", urlYt);
+            
+            await new Promise((resolve, reject) => {
+                stream.on("error", reject);
+                stream.on("finish", resolve);
+            });
+
+            let stats = fs.statSync(`./${randomName}`);
+            let fileSizeInBytes = stats.size;
+            
+            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+            
+
+
+                await client.sendMessage(
+                    m.chat, {
+                        document: fs.readFileSync(`./${randomName}`),
+                        mimetype: "audio/mpeg",
+                        fileName: titleYt + ".mp3",
+                    }, {
+                        quoted: m
+                    }
+                );
+
+            fs.unlinkSync(`./${randomName}`);
+        } catch (e) {
+            m.reply(e.toString())
         }
-
-    },
-},{quoted:m})
-
-} catch (e) {
-
-m.reply("Error occured" + e)}
-    }
+                     }
