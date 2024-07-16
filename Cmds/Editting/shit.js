d5f85672ccc5ea0698fefb1d51bfaa0effcc7566
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-16 10:59:13 +0300

    Update Editting

---
+++ Cmds/Editting/shit.js
@@ -0,0 +1,34 @@
+const canvacord = require("canvacord");
+
+module.exports = async (context) => {
+        const { client, m, Tag, botname } = context;
+
+let cap = `Converted By ${botname}`;
+
+try {
+
+        if (m.quoted) {
+            try {
+                img = await client.profilePictureUrl(m.quoted.sender, 'image')
+            } catch {
+                img = "https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg"
+            }
+                        result = await canvacord.Canvacord.shit(img);
+        } else if (Tag) {
+            try {
+                ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
+            } catch {
+                ppuser = 'https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg'
+            }
+                        result = await canvacord.Canvacord.shit(ppuser);
+        } 
+
+
+        await client.sendMessage(m.chat, { image: result, caption: cap }, { quoted: m });
+
+} catch (e) {
+
+m.reply("Something wrong occured. 😞")  
+
+}
+    }
