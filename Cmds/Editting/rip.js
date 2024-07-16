adbba962eb7e46d4494497b24c76060267ec9aab
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-16 10:44:34 +0300

    Update rip.js

--- Cmds/Editting/rip.js
+++ Cmds/Editting/rip.js
@@ -3,13 +3,15 @@ const canvacord = require("canvacord");
 module.exports = async (context) => {
         const { client, m, Tag } = context;
 
+try {
+
         if (m.quoted) {
             try {
                 img = await client.profilePictureUrl(m.quoted.sender, 'image')
             } catch {
                 img = "https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg"
             }
-                        result = await canvacord.Canvacord.wanted(img);
+                        result = await canvacord.Canvacord.rip(img);
         } else if (Tag) {
             try {
                 ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
@@ -21,4 +23,10 @@ module.exports = async (context) => {
 
 
         await client.sendMessage(m.chat, { image: result }, { quoted: m });
+
+} catch (e) {
+
+m.reply("Something wrong occured. 😞")  
+
+}
     }
