5612b6c41d539d9b17b0372b01114745e0c89bd0
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-16 10:55:49 +0300

    Update Editting

---
+++ Cmds/Editting/wasted.js
@@ -0,0 +1,49 @@
+const { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter')
+
+
+const canvacord = require("canvacord");
+
+module.exports = async (context) => {
+        const { client, m, Tag, botname } = context;
+
+let cap = `Converted By ${botname}`;
+
+try {
+
+        if (m.quoted) {
+            try {
+                img = await client.profilePictureUrl(m.quoted.sender, 'image')
+            } catch {
+                img = "https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg"
+            }
+                        result = await canvacord.Canvacord.wasted(img);
+        } else if (Tag) {
+            try {
+                ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
+            } catch {
+                ppuser = 'https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg'
+            }
+                        result = await canvacord.Canvacord.wasted(ppuser);
+        } 
+
+
+        let sticker = new Sticker(result, {
+            pack: `Triggred`,
+            author:"" ,
+            categories: ['🤩', '🎉'],
+            id: '12345',
+            quality: 75,
+            background: 'transparent' // The sticker background color (only for full stickers)
+        })
+        const stikk = await sticker.toBuffer()
+       await client.sendMessage(m.chat, {sticker: stikk}, {quoted: m})
+
+
+        
+
+} catch (e) {
+
+m.reply("Something wrong occured. 😞")  
+
+}
+    }
