861e5048ff0f492b0b471ebebbce1d52f739b3b8
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-16 11:59:30 +0300

    Update General

---
+++ Cmds/General/profilegc.js
@@ -0,0 +1,33 @@
+module.exports = async (context) => {
+        const { client, m } = context;
+
+
+function convertTimestamp(timestamp) {
+  const d = new Date(timestamp * 1000);
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return {
+    date: d.getDate(),
+    month: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(d),
+    year: d.getFullYear(),
+    day: daysOfWeek[d.getUTCDay()],
+    time: `${d.getUTCHours()}:${d.getUTCMinutes()}:${d.getUTCSeconds()}`
+  }
+}
+
+if (!m.isGroup) return m.reply("This command is meant for groups");
+
+let info = await client.groupMetadata(m.chat);
+
+let ts = await convertTimestamp(info.creation);
+
+try {
+        pp = await sock.profilePictureUrl(chat, 'image');
+      } catch {
+        pp = 'https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg';
+      }
+
+await client.sendMessage(m.chat, { image: { url: pp }, 
+          caption: `_Name_ : *${info.subject}*\n_ID_ : *${info.id}*\n_Group owner_ : *${'@'+info.owner.split('@')[0]}*\n_Group created_ : *${ts.day}, ${ts.date} ${ts.month} ${ts.year}, ${ts.time}*\n_Participants_ : *${info.size}*\n_Members_ : *${info.participants.filter((p) => p.admin == null).length}*\n_Admins_ : *${Number(info.participants.length - info.participants.filter((p) => p.admin == null).length)}*\n_Who can send message_ : *${info.announce == true ? 'Admins' : 'Everyone'}*\n_Who can edit group info_ : *${info.restrict == true ? 'Admins' : 'Everyone'}*\n_Who can add participants_ : *${info.memberAddMode == true ? 'Everyone' : 'Admins'}*`
+        }, {quoted: m })
+
+}
