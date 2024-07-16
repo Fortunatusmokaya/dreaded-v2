a3c0b58f5717e8cf96b59b252fe956b8721d57c0
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-16 10:05:41 +0300

    Update Editting

---
+++ Cmds/Editting/rip.js
@@ -0,0 +1,24 @@
+const canvacord = require("canvacord");
+
+module.exports = async (context) => {
+        const { client, m, Tag } = context;
+
+        if (m.quoted) {
+            try {
+                img = await client.profilePictureUrl(m.quoted.sender, 'image')
+            } catch {
+                img = "https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg"
+            }
+            result = await canvacord.Canvacord.rip(img);
+        } else if (mentionByTag) {
+            try {
+                ppuser = await client.profilePictureUrl(Tag[0] || m.sender, 'image')
+            } catch {
+                ppuser = 'https://telegra.ph/file/9521e9ee2fdbd0d6f4f1c.jpg'
+            }
+            result = await canvacord.Canvacord.rip(ppuser);
+        } 
+
+
+        await client.sendMessage(m.chat, { image: result }, { quoted: m });
+    }
