f35a3a3f52b10a789cf94381cf3a7445615b2cbc
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-24 00:28:19 +0300

    Update Media

---
+++ Cmds/Media/gitclone.js
@@ -0,0 +1,16 @@
+module.exports = async (context) => {
+
+const { client, m, text } = context;
+
+if (!text) return m.reply(`Where is the link?`)
+if (!text.includes('github.com')) return m.reply(`Is that a GitHub repo link ?!`)
+let regex1 = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+    let [, user3, repo] = text.match(regex1) || []
+    repo = repo.replace(/.git$/, '')
+    let url = `https://api.github.com/repos/${user3}/${repo}/zipball`
+    let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    await client.sendMessage(dest, { document: { url: url }, fileName: filename+'.zip', mimetype: 'application/zip' }, { quoted: m }).catch((err) => m.reply("error"))
+
+}
+
+
