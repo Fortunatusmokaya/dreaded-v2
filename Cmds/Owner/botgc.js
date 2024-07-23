1daf7f4c8b9acf050fe0584401f749b5a22d5ff6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-24 00:55:29 +0300

    Update botgc.js

--- Cmds/Owner/botgc.js
+++ Cmds/Owner/botgc.js
@@ -1,36 +1,36 @@
-const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
 
 module.exports = async (context) => {
-    await ownerMiddleware(context, async () => {
-        const { client, m, text, Owner } = context;
+  await ownerMiddleware(context, async () => {
+    const { client, m, text, Owner } = context;
 
-try {
+  try {
 
-            let getGroupzs = await client.groupFetchAllParticipating();
-            let groupzs = Object.entries(getGroupzs)
-                .slice(0)
-                .map((entry) => entry[1]);
-            let anaa = groupzs.map((v) => v.id);
-            let jackhuh = `Bot groups:-\n\n`
-            await m.reply(`Bot is in ${anaa.length} groups, fetching and sending their jids!`)
-            for (let i of anaa) {
-                let metadat = await client.groupMetadata(i);
-                await sleep(500)
-                jackhuh += `Subject:- ${metadat.subject}\n`
-                jackhuh += `Members: ${metadat.participants.length}\n`
-                jackhuh += `Jid:- ${i}\n\n`
+      let getGroupzs = await client.groupFetchAllParticipating();
+      let groupzs = Object.entries(getGroupzs)
+          .slice(0)
+          .map((entry) => entry[1]);
+      let anaa = groupzs.map((v) => v.id);
+      let jackhuh = `Bot groups:-\n\n`
+      await m.reply(`Bot is in ${anaa.length} groups, fetching and sending their jids!`)
+      const promises = anaa.map((i) => {
+        return new Promise((resolve) => {
+          client.groupMetadata(i).then((metadat) => {
+            setTimeout(() => {
+              jackhuh += `Subject:- ${metadat.subject}\n`
+              jackhuh += `Members: ${metadat.participants.length}\n`
+              jackhuh += `Jid:- ${i}\n\n`
+              resolve()
+            }, 500);
+          })
+        })
+      })
+      await Promise.all(promises)
+      m.reply(jackhuh);
 
-            }
-          m.reply(jackhuh);
+  } catch (e) {
+    m.reply("Error occured while accessing bot groups.\n\n" + e)
+  }
 
-
-} catch (e) {
-
-m.reply("Error occured while accessing bot groups" + e)
-
+  });
 }
-
-});
-
-}
-
