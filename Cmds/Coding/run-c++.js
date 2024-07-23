f6338f179e873b23e369ba5e8bbc868e40060d76
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-24 01:31:41 +0300

    Update Coding

---
+++ Cmds/Coding/run-c++.js
@@ -0,0 +1,29 @@
+module.exports = async (context) => {
+    const { m } = context;
+
+const {c, cpp, node, python, java} = require('compile-run');
+
+    if (m.quoted && m.quoted.text) {
+        const code = m.quoted.text;
+
+async function runCode() {
+  try {
+    let result = await cpp.runSource(code);
+    console.log(result);
+    m.reply(result.stdout);
+    m.reply(result.stderr);
+  } catch (err) {
+    console.log(err);
+    m.reply(err.stderr);
+  }
+}
+
+runCode();
+
+} else { 
+
+m.reply('Quote a valid and short C++ code to compile')
+
+}
+
+}
