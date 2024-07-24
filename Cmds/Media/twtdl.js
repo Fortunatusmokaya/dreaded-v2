8d29a836b4b930b935f0199aa3dbf77b78d548a4
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-07-24 10:26:39 +0300

    Update Media

---
+++ Cmds/Media/twtdl.js
@@ -0,0 +1,25 @@
+module.exports = async (context) => {
+
+const { client, m, text, botname } = context;
+
+if (!text) return m.reply("Provide a twitter or X link for the video");
+
+
+
+try {
+
+const response = await fetch(`https://api.prabath-md.tech/api/twitterdl?url=${text}`);
+const data = await response.json();
+
+
+const twtvid = data.data.SD;
+
+await client.sendMessage(m.chat,{video : {url : twtvid },caption : `Downloaded by ${botname}`,gifPlayback : false },{quoted : m}) 
+
+} catch (e) {
+
+m.reply("An error occured. API might be down\n" + e)
+
+}
+
+}
