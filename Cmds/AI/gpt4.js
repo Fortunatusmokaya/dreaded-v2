715017b26053132f0d007226bcf9e41769c1db4a
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-08-28 15:40:21 +0300

    Update gpt4.js

--- Cmds/AI/gpt4.js
+++ Cmds/AI/gpt4.js
@@ -10,7 +10,7 @@ const ai = require('unlimited-ai');
 const model = 'gpt-4'; 
 
 
-const messages = [ { role: 'user', content: 'Hello!' }, { role: 'system', content: 'You are an assistant' } ]; 
+const messages = [ { role: 'user', content: text }, { role: 'system', content: 'You are an assistant in WhatsApp. You are called Dreaded. You respond to user commands.' } ]; 
 
 await m.reply(await ai.generate(model, messages)); 
 
