7a1beafb25b0c83f44a6499c27c7afb083c3cb60
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-08-30 20:16:32 +0300

    Update carbon.js

--- Cmds/Coding/carbon.js
+++ Cmds/Coding/carbon.js
@@ -1,48 +1,32 @@
-  module.exports = async (context) => {
-        const { client, m, text, botname } = context; 
+module.exports = async (context) => {
+  const { client, m, text, botname } = context;
 
+  let cap = `Converted By ${botname}`;
 
-let cap = `Converted By ${botname}`;
+  if (m.quoted && m.quoted.text) {
+    const forq = m.quoted.text;
 
- if (m.quoted && m.quoted.text) {
-        const forq = m.quoted.text;
+    try {
+      let response = await fetch('https://carbonara.solopov.dev/api/cook', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          code: forq,
+          backgroundColor: '#1F816D',
+        }),
+      });
 
+      if (!response.ok) return m.reply('API failed to fetch a valid response.')
 
-try {
+      let per = await response.buffer();
 
-
-let response = await fetch('https://carbonara.solopov.dev/api/cook', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        code: forq,
-        backgroundColor: '#1F816D',
-      }),
-    });
-
-
-if (!response.ok) return m.reply('API failed to fetch a valid response.')
-    
-
-let per = await response.buffer();
-
-
-        await client.sendMessage(m.chat, { image: per, caption: cap }, { quoted: m });
-
-
-} else { m.reply('Quote a code message');
-
+      await client.sendMessage(m.chat, { image: per, caption: cap }, { quoted: m });
+    } catch (error) {
+      m.reply("An error occured\n" + error)
+    }
+  } else {
+    m.reply('Quote a code message');
+  }
 }
-
-} catch (error) { 
-
-m.reply("An error occured\n" + error)
-
-}
-
-}
-
-
-
