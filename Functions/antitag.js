7d5b97150e339be6ee3afdd766aad90406065b92
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-05 15:27:38 +0300

    Update antitag.js

--- Functions/antitag.js
+++ Functions/antitag.js
@@ -3,14 +3,24 @@ module.exports = async (client, m, isBotAdmin, itsMe, isAdmin, Owner, body, anti
 
 if (m.isGroup && antitag === 'true' && !Owner && isBotAdmin && !isAdmin && m.mentionedJid && m.mentionedJid.length > 10) {
 if (itsMe) return;
+const kid = m.sender;
+
+await client.sendMessage(m.chat, {text:`@${kid.split("@")[0]}, do not tag!`, contextInfo:{mentionedJid:[kid]}}, {quoted:m}); 
+
+        
+        await client.sendMessage(m.chat, {
+            delete: {
+                remoteJid: m.chat,
+                fromMe: false,
+                id: m.key.id,
+                participant: kid
+            }
+        });
 
 
+await client.groupParticipantsUpdate(m.chat, [kid], 'remove')
 
 
-await client.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-
-cona = m.sender;
-await client.sendMessage(m.chat, {text:`@${cona.split("@")[0]}, Do not tag!`, contextInfo:{mentionedJid:[cona]}}, {quoted:m}); 
 
 }
 
