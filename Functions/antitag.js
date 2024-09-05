fd3b3471617a0599c53fcc12aa16057e225822df
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-05 15:11:01 +0300

    Update antitag.js

--- Functions/antitag.js
+++ Functions/antitag.js
@@ -7,7 +7,7 @@ if (itsMe) return;
 
 await m.reply("Tagall/Hidetag detected");
 
-await client.groupParticipantsUpdate(m.chat, [sender], 'remove')
+await client.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 cona = m.sender;
 await client.sendMessage(m.chat, {text:`@${cona.split("@")[0]}, Do not tag!`, contextInfo:{mentionedJid:[cona]}}, {quoted:m}); 
 
