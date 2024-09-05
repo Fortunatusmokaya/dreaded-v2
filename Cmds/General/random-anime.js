6c2ab22fb597f805291cc4917e9667b7424795e1
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-05 19:11:48 +0300

    Update General

---
+++ Cmds/General/random-anime.js
@@ -0,0 +1,29 @@
+module.exports = async (context) => {
+        const { client, m, text } = context;
+
+const axios = require("axios");
+
+  const link = "https://api.jikan.moe/v4/random/anime";
+
+  try {
+    const response = await axios.get(link);
+    const data = response.data.data;
+
+    const title = data.title;
+    const synopsis = data.synopsis;
+    const imageUrl = data.images.jpg.image_url;
+    const episodes = data.episodes;
+    const status = data.status;
+
+   
+
+    const message = `📺 Title: ${title}\n🎬 Épisodes: ${episodes}\n📡 Status: ${status}\n📝 Synopsis: ${synopsis}\n🔗 URL: ${data.url}`;
+
+   
+    await client.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { quoted: ms });
+  } catch (error) {
+    
+   m.reply('An error occured.');
+  }
+
+}
