43ab5daf5346a8ba41467a833c8f61f49e94f005
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-05 15:55:54 +0300

    Update Functions

---
+++ Functions/antidelete.js
@@ -0,0 +1,17 @@
+module.exports = async (client, m, store, chatUpdate, antidelete) => {
+    if (m.mtype == 'protocolMessage' && antidelete === 'true') {
+        if (m.fromMe) return;
+
+const mokaya = chatUpdate.messages[0].message.protocolMessage
+
+if (store.messages && store.messages[m.chat] && store.messages[m.chat].array) {
+
+const chats = store.messages[m.chat].array.find(a => a.id === mokaya.key.id);
+
+chats.msg.contextInfo = { mentionedJid: [chats.key.participant], isForwarded: true, forwardingScore: 1, quotedMessage: { conversation: 'Deleted Message'}, ...chats.key }
+
+await client.relayMessage(client.user.id, { [chats.type]: chats.msg }, {})
+				}
+			}
+		}
+}
