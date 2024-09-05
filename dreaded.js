73bd7912880781c51188a35ce5bbf92252b4b6d6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-05 15:42:26 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -93,7 +93,7 @@ const context = {
     client, m, text, Owner, chatUpdate, store, isBotAdmin, isAdmin, IsGroup, participants,
     pushname, body, budy, totalCommands, args, mime, qmsg, msgDreaded, botNumber, itsMe,
     packname, author, generateProfilePicture, groupMetadata, dreadedspeed, mycode,
-    fetchJson, exec, getRandom, UploadFileUgu, TelegraPh, prefix, cmd, botname, mode, gcpresence, antitag,antidelete, antionce, fetchBuffer,store, Tag
+    fetchJson, exec, getRandom, UploadFileUgu, TelegraPh, prefix, cmd, botname, mode, gcpresence, antitag,antidelete, antionce, fetchBuffer,store, chatUpdate, Tag
 };
 if (cmd && mode === 'private' && !itsMe && !Owner) {
 return;
