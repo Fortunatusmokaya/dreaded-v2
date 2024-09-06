8419fdc9d4276d6f6c09b41216a9d2a6f92ea6b9
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-06 19:15:28 +0300

    Update joingc.js

--- Cmds/Owner/joingc.js
+++ Cmds/Owner/joingc.js
@@ -5,31 +5,24 @@ module.exports = async (context) => {
         const { client, m, text, Owner, args } = context;
 
                  if (!text) return m.reply("provide a valid group link")
-                let result = args[0].split('https://chat.whatsapp.com/')[1] 
+let result = args[0].split('https://chat.whatsapp.com/')[1]
 
 try {
+    const info = await client.groupGetInviteInfo(result);
+    let { subject } = info;
 
-const info = await client.groupGetInviteInfo(result);
-
-let { subject } = info;
-
+    await client.groupAcceptInvite(result)
+        .then(() => m.reply(`Bot has joined ${subject}`))
+        .catch((res) => {
+            if (res.data == 400) return m.reply('Group does not exist.');
+            if (res.data == 401) return m.reply('Bot was previously removed, cannot join using link.');
+            if (res.data == 409) return m.reply('Bot was already in the group, Uh?');
+            if (res.data == 410) return m.reply('This group link is reset, provide a new one');
+            if (res.data == 500) return m.reply('This group is full');
+        });
 } catch (error) {
-
-console.log("error")
-
+    console.log("error")
 }
-
-
-await client.groupAcceptInvite(result).catch((res) => {
-					if (res.data == 400) return m.reply('Group does not exist.');
-					if (res.data == 401) return m.reply('Bot was previously removed, cannot join using link.');
-					if (res.data == 409) return m.reply('Bot was already in the group, Uh?');
-					if (res.data == 410) return m.reply('This group link is reset, provide a new one');
-					if (res.data == 500) return m.reply('This group is full');
-
-if (res.data == 200) m.reply(`Bot has joined ${subject}`)
-
-				})
                  
 
              });
