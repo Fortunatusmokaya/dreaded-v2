fe3ce39e028dfe378fba6347522b2d947ebaf831
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-19 20:43:02 +0300

    Update upload.js

--- Cmds/Media/upload.js
+++ Cmds/Media/upload.js
@@ -1,17 +1,42 @@
 module.exports = async (context) => {
-    const { client, m, mime, UploadFileUgu, TelegraPh, qmsg } = context;
+    const { client, m, uploadtoimgur } = context;
  const fs = require("fs");
+const path = require('path');
+
 const util = require("util");
 
-                let fta2 = await client.downloadAndSaveMediaMessage(qmsg)
-                if (/image/.test(mime)) {
-                    let fta = await TelegraPh(fta2)
-                    m.reply(util.format(fta))
-                } else if (!/image/.test(mime)) {
-                    let fta = await UploadFileUgu(fta2)
-                    m.reply(util.format(fta))
-                }
-                await fs.unlinkSync(fta2)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+
+if (!mime) return m.reply('Quote an image or video')
+
+let mediaBuffer = await q.download()
+
+  if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.')
+
+
+
+
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+
+
+if (isTele) {
+
+let fta2 = await client.downloadAndSaveMediaMessage(q)
+
+    let link = await uploadtoimgur(fta2)
+
+    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2)
+
+    m.reply(`Media Link:-\n\n${link}`)
+  } else {
+    m.reply(`Error occured...`)
+  }
+              
+      await fs.unlinkSync(fta2)
+          
+                
+
 
             }
 
