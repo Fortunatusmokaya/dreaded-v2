cfc05f2ab652f8ae77d85bc8016679b6a5e48653
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-20 19:23:24 +0300

    Update del.js

--- Cmds/General/del.js
+++ Cmds/General/del.js
@@ -1,9 +1,15 @@
 module.exports = async (context) => {
     const { client, m, prefix } = context;
 
+
+
+
+if (!m.quoted) return m.reply('Quote a message sent by bot');
+
 if (m.quoted || m.quoted.fromMe === false) {
   return m.reply(`I cannot delete other users' messages, you can still delete using ${prefix}delete command`);
 }
+
 
 await m.quoted.delete()
 }
