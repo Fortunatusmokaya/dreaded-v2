e218a9652a38966705b2f95272296e3cf42bf2ef
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-20 15:35:45 +0300

    Update del.js

--- Cmds/General/del.js
+++ Cmds/General/del.js
@@ -1,7 +1,9 @@
 module.exports = async (context) => {
     const { client, m, prefix } = context;
 
-if (!m.quoted && !m.quoted.fromMe) return m.reply(`I cannot delete other users message, you can still delete it using ${prefix}delete command`);
+if (!m.quoted || m.quoted.fromMe === false) {
+  return m.reply(`I cannot delete other users' messages, you can still delete it using ${prefix}delete command`);
+}
 
 await m.quoted.delete()
 }
