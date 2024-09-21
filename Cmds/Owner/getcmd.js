afe477d6a7acfd45f4ea4333596ccad254d6791d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-21 18:07:18 +0300

    Update Owner

---
+++ Cmds/Owner/getcmd.js
@@ -0,0 +1,34 @@
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
+const fs = require('fs');
+module.exports = async (context) => {
+    await ownerMiddleware(context, async () => {
+        const { client, m, text, Owner, prefix } = context;
+
+if (!text) return m.reply(`Provide name of command to fetch it's code. Like, ${prefix}getcmd joingc`);
+
+const fs = require('fs');
+const categories = [
+    { name: 'AI' },
+    { name: 'General' },
+    { name: 'Media' },
+    { name: 'Editting' },
+    { name: 'groups' },
+    { name: 'Owner' },
+    { name: 'Coding' }
+];
+
+for (const category of categories) {
+    const fileName = text
+    const filePath = `./cmds/${category.name}/${fileName.endsWith('.js') ? fileName : `${fileName}.js`}`;
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            m.reply(`Error reading file: ${err.message}`);
+        } else {
+            m.reply(`\`\`\`${data}\`\`\``);
+        }
+    });
+}
+
+});
+
+}
