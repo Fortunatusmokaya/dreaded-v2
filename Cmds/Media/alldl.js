0a2425f8e96057495510e58481bcc7a8dce8d363
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-21 09:26:13 +0300

    Update alldl.js

--- Cmds/Media/alldl.js
+++ Cmds/Media/alldl.js
@@ -1,23 +1,26 @@
 module.exports = async (context) => {
 
 const { client, m, text, botname } = context;
+const { alldown } = require("nayan-media-downloader");
 
 try {
 
 if (!text) return m.reply("Provide any media link...");
 
-const {alldown} = require("nayan-media-downloader");
-const url = text
 
 
-  const data = await alldown(url);
+
+
+  const data = await alldown(text);
 const title = data.media.title;
+const low = data.media.low;
 
 await m.reply(title)
+await m.reply(low)
 
 } catch (er) {
 
-m.reply(er)
+await m.reply('Error\n' + er)
 
 }
 
