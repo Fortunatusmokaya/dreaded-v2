114b9418d486beb39c6e423a37372820ce53a96c
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-21 18:32:31 +0300

    Update getcmd.js

--- Cmds/Owner/getcmd.js
+++ Cmds/Owner/getcmd.js
@@ -1,4 +1,4 @@
-const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
+/* const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
 const fs = require('fs');
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
@@ -32,3 +32,51 @@ for (const category of categories) {
 });
 
 }
+
+*/
+
+module.exports = async (context) => {
+    await ownerMiddleware(context, async () => {
+        const { client, m, text, Owner, prefix } = context;
+
+        if (!text) return m.reply(`Provide name of command to fetch it's code. Like, ${prefix}getcmd fullpp`);
+
+        const categories = [
+            { name: 'AI' },
+            { name: 'General' },
+            { name: 'Media' },
+            { name: 'Editting' },
+            { name: 'groups' },
+            { name: 'Owner' },
+            { name: 'Coding' }
+        ];
+
+        let fileFound = false;
+
+        for (const category of categories) {
+            const fileName = text
+            const filePath = `./Cmds/${category.name}/${fileName.endsWith('.js') ? fileName : `${fileName}.js`}`;
+
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    // File doesn't exist, move on to the next category
+                    return;
+                }
+
+                // File exists, read it and reply with the contents
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        m.reply(`Error reading file:-\n ${err.message}`);
+                    } else {
+                        m.reply(`\`\`\`${data}\`\`\``);
+                        fileFound = true;
+                    }
+                });
+            });
+        }
+
+        if (!fileFound) {
+            m.reply(`Command not found: ${text}`);
+        }
+    });
+};
