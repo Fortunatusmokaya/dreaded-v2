8ef168007ad4b15f2a034773108f93ad3381a5de
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-09-26 18:40:33 +0300

    Update General

---
+++ Cmds/General/vcf.js
@@ -0,0 +1,28 @@
+        const { client, m, participants, text } = context;
+
+
+module.exports = async (context) => {
+const fs = require("fs");
+let gcdata = await client.groupMetadata(m.chat)
+let gcmem = participants.map(a => a.id)
+
+let vcard = ''
+let noPort = 0
+
+for (let a of gcdata.participants) {
+    vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:[${noPort++}] +${a.id.split("@")[0]}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`
+}
+
+let cont = './contacts.vcf'
+
+await m.reply('A moment, dreaded is compiling '+gcdata.participants.length+' contacts into a vcf...');
+
+await fs.writeFileSync(cont, vcard.trim())
+
+await client.sendMessage(m.chat, {
+    document: fs.readFileSync(cont), mimetype: 'text/vcard', fileName: 'Group contacts.vcf', caption: 'VCF for '+gcdata.subject+'\n'+gcdata.participants.length+' contacts'
+}, {ephemeralExpiration: 86400, quoted: m})
+fs.unlinkSync(cont)
+
+
+}
