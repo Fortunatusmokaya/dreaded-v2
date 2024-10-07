1b509ca7996f45a1317c10bfce695f1b5e818315
STRIKERBOY <110353280+Devenlee350@users.noreply.github.com>
2024-10-08 00:42:08 +0200

    𝚂𝚃𝚁𝙸𝙺𝙴𝚁𝙱𝙾𝚈

--- Cmds/Owner/leavegc.js
+++ Cmds/Owner/leavegc.js
@@ -7,7 +7,7 @@ module.exports = async (context) => {
 
 if (!m.isGroup) return m.reply("This command is meant for groups");
 
-await client.sendMessage(m.chat, { text : 'Goodbye 👋, Dreaded will now exit the group...' , mentions: participants.map(a => a.id)}, { quoted: m });
+await client.sendMessage(m.chat, { text : 'Goodbye 👋, sᴛʀɪᴋᴇʀʙᴏʏᵇᵒᵗ will now exit the group...' , mentions: participants.map(a => a.id)}, { quoted: m });
 await client.groupLeave(m.chat); 
 
 })
