549a7d9fd44776a7b3246a8e97666e655dd02aa2
Devenlee250 <devenleekudzai@gmail.com>
2024-10-27 15:36:54 +0200

    Update retrieve.js

--- Cmds/General/retrieve.js
+++ Cmds/General/retrieve.js
@@ -12,11 +12,11 @@ if (m.quoted.message) {
             if (/video/.test(type)) {
 
 
-               await client.sendMessage(m.chat, { video: media, caption: `Retrieved by Dreaded! 🦄\nOriginal caption: ${q.caption}`}, { quoted: m})
+               await client.sendMessage(m.chat, { video: media, caption: `Retrieved by sᴛʀɪᴋᴇʀʙᴏʏ_вσt! ⌤\nOriginal caption: ${q.caption}`}, { quoted: m})
 
             } else if (/image/.test(type)) {
 
-await client.sendMessage(m.chat, { image: media, caption: `Retrieved by Dreaded! 🦄\nOriginal caption: ${q.caption}`}, { quoted: m})
+await client.sendMessage(m.chat, { image: media, caption: `Retrieved by sᴛʀɪᴋᴇʀʙᴏʏ_вσt! ⌤\nOriginal caption: ${q.caption}`}, { quoted: m})
 
             }
          } else m.reply("That is not a viewonce media. . .")
