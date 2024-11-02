988a2a98f736a9cc66a5531569884c76c97ec492
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-02 21:01:10 +0300

    Update Utils

---
+++ Cmds/Utils/screenshot.js
@@ -0,0 +1,23 @@
+module.exports = async (context) => {
+
+const { client, m, text, botname } = context;
+
+
+
+try {
+let cap = `Screenshot by ${botname}`
+
+if (!text) return m.reply("Provide a website link to screenshot.")
+
+const image = `https://image.thum.io/get/fullpage/${text}`
+
+await client.sendMessage(m.chat, { image: { url: image }, caption: cap}, {quoted: m });
+
+
+} catch (error) {
+
+m.reply("An error occured.")
+
+}
+
+}
