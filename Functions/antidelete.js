75d1cf4c6918e9f89c6508129d1b61486c24c46d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-03 09:09:00 +0300

    Update antidelete.js

--- Functions/antidelete.js
+++ Functions/antidelete.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = async (client, m, antidelete) => {
-    if (antidelete === 'true' && m.message.protocolMessage && m.message.protocolMessage.type === 0) {
+    if (m.isGroup && antidelete === 'true' && m.message.protocolMessage && m.message.protocolMessage.type === 0) {
         console.log("Deleted Message Detected!");
         let key = m.message.protocolMessage.key;
 
