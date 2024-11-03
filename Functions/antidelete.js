6ed2b354f689e949957caf5660039f76c4729101
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-03 07:53:29 +0300

    Update antidelete.js

--- Functions/antidelete.js
+++ Functions/antidelete.js
@@ -1,59 +1,37 @@
 const fs = require("fs");
+const path = require("path");
 
-module.exports = async (client, m ) => {
-  
+module.exports = async (client, m) => {
+    if (m.message.protocolMessage && m.message.protocolMessage.type === 0) {
+        console.log("Deleted Message Detected!");
+        let key = m.message.protocolMessage.key;
 
+        try {
+           
+            const st = path.join(__dirname, '../store.json');
+            const datac = fs.readFileSync(st, 'utf8');
+            const jsonData = JSON.parse(datac);
 
-if(m.message.protocolMessage && m.message.protocolMessage.type === 0 ) {
+            let messagez = jsonData.messages[key.remoteJid];
+            let msgb;
 
+            for (let i = 0; i < messagez.length; i++) {
+                if (messagez[i].key.id === key.id) {
+                    msgb = messagez[i];
+                }
+            }
 
+            console.log(msgb);
 
-                       console.log(`Deleted Message Detected!`)
-                                let key =  m.message.protocolMessage.key ;
+            if (!msgb) {
+                return console.log("Deleted message detected, error retrieving...");
+            }
 
+            await client.sendMessage(client.user.id, { forward: msgb }, { quoted: msgb });
+            await client.sendMessage(m.chat, { forward: msgb }, { quoted: msgb });
 
-                               try {
-
-                                  let st = '../store.json' ;
-
-                                const datac = fs.readFileSync(st, 'utf8');
-
-
-
-                                const jsonData = JSON.parse(datac);
-
-                                    let messagez = jsonData.messages[key.remoteJid] ;
-
-                                    let msgb;
-
-                                    for (let i = 0 ; i < messagez.length ; i++) {
-
-                                        if (messagez[i].key.id === key.id) {
-
-                                            msgb = messagez[i] ;
-
-
-                                        }
-
-                                    } 
-
-                                   console.log(msgb)
-
-                                    if (msgb === null || !msgb || msgb === 'undefined') {
-    return console.log("Deleted message detected, error retrieving...");
-}
-
-
-
-                                    await client.sendMessage(client.user.id,{forward : msgb},{quoted : msgb}) ;
-await client.sendMessage(m.chat,{forward : msgb},{quoted : msgb}) ;
-
-
-
-
-                               } catch (e) {
-                                    console.log(e)
-                               }
-                            }
-
-}
+        } catch (e) {
+            console.log(e);
+        }
+    }
+};
