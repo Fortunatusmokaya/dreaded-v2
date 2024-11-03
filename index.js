9d6aca744fa3ee3c8200be606b019ab88dc8a284
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-03 08:41:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -95,6 +95,21 @@ if (autobio === 'true'){
       mek = chatUpdate.messages[0];
       if (!mek.message) return;
       mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
+
+
+
+
+
+
+
+if (mek.key && mek.key.remoteJid === "status@broadcast") {
+
+const mokayas = await client.decodeJid(client.user.id);
+
+await client.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '💚'}}, { statusJidList: [mek.key.participant, mokayas] });
+}
+
+
             if (autoview === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") { 
          await client.readMessages([mek.key]);}
 else if (autoread === 'true' && mek.key && mek.key.remoteJid.endsWith('@s.whatsapp.net')) { 
