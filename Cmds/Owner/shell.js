9c6cb117dd4a857196a1da037b8cb0e69f5a1e63
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-08 01:51:55 +0300

    Update Owner

---
+++ Cmds/Owner/shell.js
@@ -0,0 +1,18 @@
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
+
+module.exports = async (context) => {
+  await ownerMiddleware(context, async () => {
+
+        const { client, m, text, budy} = context;
+
+const { exec, spawn, execSync } = require("child_process");
+
+    
+  if (!text) return m.reply("Provide a shell command to execute!")
+  exec(budy.slice(2), (err, stdout) => {
+if(err) return m.reply(err)
+if (stdout) return m.reply(stdout)
+  })
+   });
+
+}
