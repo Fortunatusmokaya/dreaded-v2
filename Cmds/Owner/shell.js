56f0ad706b51e4f24f9a1efc0520a07893081380
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-08 01:56:01 +0300

    Update shell.js

--- Cmds/Owner/shell.js
+++ Cmds/Owner/shell.js
@@ -9,7 +9,7 @@ const { exec, spawn, execSync } = require("child_process");
 
     
   if (!text) return m.reply("Provide a shell command to execute!")
-  exec(budy.slice(2), (err, stdout) => {
+  exec(text.slice(2), (err, stdout) => {
 if(err) return m.reply(err)
 if (stdout) return m.reply(stdout)
   })
