386e917c743e9b39860e52bc121b084fb3776242
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-16 16:00:37 +0300

    Update alldl.js

--- Cmds/Media/alldl.js
+++ Cmds/Media/alldl.js
@@ -1,60 +1,25 @@
 module.exports = async (context) => {
 
-const { client, m, text, botname } = context;
-const { alldown } = require("nayan-media-downloader");
+const { client, m, text, botname, fetchJson } = context;
+
+if (!text) return m.reply("Provide any link for download.\nE.g:- FB, X, tiktok, capcut etc");
+
+
 
 try {
 
-if (!text) return m.reply("Provide any media link..nn .");
-
-alldown(text).then(data => {
-  if (data && data.media && data.media.title) {
-    m.reply(data.media.title);
-  } else {
-    console.error('Error: data.media.title is undefined');
-    m.reply('An error occurred');
-  }
-}).catch(error => {
-  console.error('Error:', error);
-  m.reply('An error occurred' + error);
-});
+const data = await fetchJson(`https://api.dreaded.site/api/alldl?url=${text}`);
 
 
 
-/*   const data = await alldown(text);
+const allvid = data.data.videoUrl;
 
+await client.sendMessage(m.chat,{video : {url : allvid },caption : `Downloaded by ${botname}`,gifPlayback : false },{quoted : m}) 
 
+} catch (e) {
 
-if (data && data.media) {
-
-
-
-const info = data.media.title;
-await m.reply('hey');
-
-  await m.reply(info);
- 
-} else { m.reply("data inaccessible")
-}
-*/
-
-
-
-
-
-
-} catch (er) {
-
-await m.reply('Error\n' + er)
+m.reply("An error occured. API might be down\n" + e)
 
 }
 
 }
-
-
-  
-
-
-
-
-
