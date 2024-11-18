dc4878a487d454e01a5b0dda90139812ea0de08d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-11-18 20:18:31 +0300

    Update tikdl.js

--- Cmds/Media/tikdl.js
+++ Cmds/Media/tikdl.js
@@ -4,43 +4,43 @@ const path = require("path");
 const axios = require("axios");
 
 module.exports = async (context) => {
-    const { client, m, text } = context;
+    const { client, m, text, fetchJson } = context;
 
     try {
-        
+       
         if (!text) return m.reply("Provide a TikTok link for the video.");
         if (!text.includes("tiktok.com")) return m.reply("That is not a valid TikTok link.");
 
        
-        const response = await axios.get(`https://api.dreaded.site/api/tiktok?url=${text}`);
+        const data = await fetchJson(`https://api.dreaded.site/api/tiktok?url=${text}`);
 
-        if (response.status !== 200 || !response.data.tiktok || !response.data.tiktok.video) {
+        if (!data || data.status !== 200 || !data.tiktok || !data.tiktok.video) {
             return m.reply("We are sorry, but the API endpoint didn't respond correctly. Try again later.");
         }
 
-        const tikVideoUrl = response.data.tiktok.video;
-        const videoDesc = response.data.tiktok.description;
+        const tikVideoUrl = data.tiktok.video;
+        const videoDesc = data.tiktok.description;
 
         
         const outputFileName = `TikTok_${Date.now()}.mp4`;
         const outputPath = path.join(__dirname, outputFileName);
 
-        
-        const videoStream = await axios({
+      
+        const response = await axios({
             url: tikVideoUrl,
             method: "GET",
             responseType: "stream",
         });
 
-        if (videoStream.status !== 200) {
+        if (response.status !== 200) {
             return m.reply("Failed to download the TikTok video. Please try again later.");
         }
 
-        ffmpeg(videoStream.data)
+        ffmpeg(response.data)
             .toFormat("mp4")
             .save(outputPath)
             .on("end", async () => {
-             
+              
                 await client.sendMessage(
                     m.chat,
                     {
