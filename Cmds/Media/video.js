76073e5d4e7a1d30501396c9432e0746ee5fafdc
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-02 21:17:00 +0300

    Update video.js

--- Cmds/Media/video.js
+++ Cmds/Media/video.js
@@ -9,14 +9,14 @@ if (!text) return m.reply("What video do you want to download ?")
 let search = await yts(text);
         let link = search.all[0].url;
 
-        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?url=${link}`)
+        let data = await fetchJson (`https://api.dreaded.site/api/alldl?url=${link}`)
 await client.sendMessage(m.chat, {
-  video: {url: data.result.downloadLink},
+  video: {url: data.data.videoUrl},
 mimetype: "video/mp4",
- fileName: `${data.result.title}.mp4`}, { quoted: m });
+ fileName: `${data.data.title}.mp4`}, { quoted: m });
 
 await client.sendMessage(m.chat, {
- document: {url: data.result.downloadLink},
+ document: {url: data.data.videoUrl},
 mimetype: "video/mp4",
  fileName: `${search.all[0].title}.mp4` }, { quoted: m });
 
