88502c8f67db64cc7c4713b5a5bed8c61dbf73a6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-08 18:32:59 +0300

    Update Media

---
+++ Cmds/Media/play.js
@@ -0,0 +1,30 @@
+module.exports = async (context) => {
+    const { client, m, text, fetchJson } = context;
+
+const yts = require("yt-search");
+try {
+
+if (!text) return m.reply("What song do you want to download ?")
+
+
+
+        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/audio?query=${text}`)
+
+let name = data.result.title;
+await m.reply(`_Downloading ${name}_`
+
+
+await client.sendMessage(m.chat, {
+ document: {url: data.result.downloadLink},
+mimetype: "audio/mpeg",
+ fileName: name }, { quoted: m });
+
+
+
+} catch (error) {
+
+m.reply("Download failed\n" + error)
+
+}
+
+}
