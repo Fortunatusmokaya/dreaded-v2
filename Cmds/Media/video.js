064090231ac95384b96cf2cf88c1e322addde574
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-08 18:54:36 +0300

    Update video.js

--- Cmds/Media/video.js
+++ Cmds/Media/video.js
@@ -1,1 +1,31 @@
- // hehe
+module.exports = async (context) => {
+    const { client, m, text, fetchJson } = context;
+
+const yts = require("yt-search");
+try {
+
+if (!text) return m.reply("What song do you want to download ?")
+
+
+
+        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?query=${text}`)
+
+let name = data.result.title;
+await m.reply(`_Downloading ${name}_`)
+
+
+
+
+await client.sendMessage(m.chat, {
+ video: {url: data.result.videoLink},
+mimetype: "audio/mpeg", caption: name,
+ fileName: name }, { quoted: m });
+
+
+} catch (error) {
+
+m.reply("Download failed\n" + error)
+
+}
+
+}
