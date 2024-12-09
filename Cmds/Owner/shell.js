34797d58f0303fef063072f4e215284f92e07b7b
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-09 19:09:14 +0300

    Update shell.js

--- Cmds/Owner/shell.js
+++ Cmds/Owner/shell.js
@@ -2,23 +2,34 @@ const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
 
 module.exports = async (context) => {
   await ownerMiddleware(context, async () => {
-    const { client, m, text, budy } = context;
+    const { client, m, text, budy, prefix } = context; 
 
     try {
       const { exec, spawn, execSync } = require("child_process");
 
-      if (!text || text.length <= 2) {
-        return m.reply("Provide a shell command to execute! Example: .shell ls");
+      
+      const trimmedText = text.trim();
+
+      
+
+      
+      if (!trimmedText || !trimmedText.startsWith(`${prefix}shell`) || trimmedText.length <= prefix.length + 6) {
+        return m.reply(`Provide a shell command to execute! Example: ${prefix}shell ls`);
       }
 
-      const command = text.slice(2).trim();
+      
+      const command = trimmedText.slice(prefix.length + 6).trim(); 
+
       if (!command) {
         return m.reply("No valid command provided. Please provide a valid shell command.");
       }
 
-      exec(command, (err, stdout) => {
+      exec(command, (err, stdout, stderr) => {
         if (err) {
           return m.reply(`Error: ${err.message}`);
+        }
+        if (stderr) {
+          return m.reply(`stderr: ${stderr}`);
         }
         if (stdout) {
           return m.reply(stdout);
