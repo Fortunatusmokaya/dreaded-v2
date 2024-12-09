1a7f426182561c021cb0588dc09587d6833519f4
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-09 18:09:48 +0300

    Update update.js

--- Cmds/Owner/update.js
+++ Cmds/Owner/update.js
@@ -1,40 +1,37 @@
-/* const simpleGit = require("simple-git");
-const Heroku = require("heroku-client");
-const herokuapi = process.env.HEROKUAPI || '';
-const name = process.env.HEROKUNAME || '';
-const fs = require("fs");
+ const simpleGit = require("simple-git");
 
 const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware'); 
 
 module.exports = async (context) => {
   await ownerMiddleware(context, async () => {
+
     const { client, m, text, Owner } = context;
 
-    const heroku = new Heroku({ token: herokuapi });
-    const repo = await heroku.get(`/apps/${name}/git`);
+const git = simpleGit();
+    try {
+        
+        await git.addRemote('upstream', 'https://github.com/Fortunatusmokaya/dreaded-v2.git').catch(() => {});
+        
+        
+        await git.fetch('upstream');
+        
+       
+        const commits = await git.log(['main' + '..upstream/main']);
+        if (commits.total > 0) {
+            let updateMessage = `Updates available: ${commits.total} commits\n\n`;
+            commits.all.forEach(commit => {
+                updateMessage += `● ${commit.date.substring(0, 10)}: ${commit.message} - By: ${commit.author_name}\n`;
+            });
+            await m.reply(updateMessage);
+        } else {
+            await m.reply('You are already using the latest version.');
+        }
+    } catch (error) {
+        await m.reply('Failed to check for updates. ' + error.message);
+}
 
-    // Create a temporary directory to clone the repository
-    const tempDir = await fs.promises.mkdtemp('/tmp/');
 
-    // Clone the repository
-    const git = simpleGit(tempDir);
-    await git.clone(repo.git_url);
-
-    // Fetch the latest changes
-    await git.fetch();
-
-    // Get the commit log
-    const commits = await git.log(['main..origin/main']);
-
-    if (commits.total === 0) {
-      return m.reply('✅ Your bot is up to date with main branch!');
-    } else {
-      m.reply("❌ Your bot is not up to date, update it by redeploying or syncing your fork");
-    }
-
-    // Clean up the temporary directory
-    await fs.promises.rmdir(tempDir, { recursive: true });
+    
   });
 };
 
-*/
