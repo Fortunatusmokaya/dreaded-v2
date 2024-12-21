835a929caf697e81998786f5929206eb5cbfb3e7
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-21 13:44:00 +0300

    Update status_saver.js

--- Functions/status_saver.js
+++ Functions/status_saver.js
@@ -3,17 +3,12 @@ module.exports = async (client, m, Owner, prefix) => {
   const textL = m.text.toLowerCase();
   const quotedMessage = m.msg?.contextInfo?.quotedMessage;
 
-
-
 if (quotedMessage && textL.startsWith(prefix + "save") && !m.quoted.chat.includes("status@broadcast")) {
     return m.reply("You did not tag a status media to save.");
   }
 
 
   if (Owner && quotedMessage && textL.startsWith(prefix + "save") && m.quoted.chat.includes("status@broadcast")) {
-    
-    
-
     if (quotedMessage.imageMessage) {
       let imageCaption = quotedMessage.imageMessage.caption;
       let imageUrl = await client.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
