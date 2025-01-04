4a797ef74c937540a474ae5d77799777ba36f621
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-05 02:33:21 +0300

    Update play.js

--- Cmds/Media/play.js
+++ Cmds/Media/play.js
@@ -1,30 +1,35 @@
+const axios = require("axios");
+
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
 
-const yts = require("yt-search");
-try {
+    const yts = require("yt-search");
 
-if (!text) return m.reply("What song do you want to download ?")
+    try {
+        if (!text) return m.reply("What song do you want to download?");
+            const {
+                videos
+            } = await yts(text);
+            if (!videos || videos.length <= 0) {
+                m.reply(`No songs found!`)
+                return;
+            }
+            let urlYt = videos[0].url
+
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${urlYt}`);
+        let name = data.title;
+        let audio = data.audioUrl;
+
+        await m.reply(`_Downloading ${name}_`);
 
 
-
-        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?query=${text}`)
-
-let name = data.result.title;
-await m.reply(`_Downloading ${name}_`)
-
-
-await client.sendMessage(m.chat, {
- document: {url: data.result.audioLink},
+        await client.sendMessage(m.chat, {
+ document: {url: audio },
 mimetype: "audio/mpeg",
  fileName: name }, { quoted: m });
 
 
-
-} catch (error) {
-
-m.reply("Download failed\n" + error)
-
-}
-
-}
+    } catch (error) {
+        m.reply("Download failed\n" + error.message);
+    }
+};
