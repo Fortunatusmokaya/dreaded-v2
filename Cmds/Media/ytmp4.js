71c7141fdf30137588b22ec0b294cd91494f5ec0
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-05 02:47:14 +0300

    Update ytmp4.js

--- Cmds/Media/ytmp4.js
+++ Cmds/Media/ytmp4.js
@@ -1,34 +1,37 @@
+const axios = require("axios");
+
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
 
-try {
+    let urls = text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch\?v=|v\/|embed\/|shorts\/|playlist\?list=)?)([a-zA-Z0-9_-]{11})/gi);
+        if (!urls) return m.reply('provide a valid YouTube link, eh ?');
 
-if (!text) return m.reply("Where is the YouTube link ?")
-
-        let urls = text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch\?v=|v\/|embed\/|shorts\/|playlist\?list=)?)([a-zA-Z0-9_-]{11})/gi);
-        if (!urls) return m.reply('Is this a YouTube link ?');
-        let urlIndex = parseInt(text) - 1;
-        if (urlIndex < 0 || urlIndex >= urls.length)
-                return m.reply('Invalid URL.');
+    try {
 
 
-        let data = await fetchJson(`https://api.dreaded.site/api/alldl?url=${text}`);
-        
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/video?url=${text}`);
+        let name = data.title;
+        let video = data.videoUrl;
+
+        await m.reply(`_Downloading ${name}_`);
+
+
+                await client.sendMessage(m.chat, {
+ video: {url: video},
+mimetype: "video/mp4", caption: name,
+ fileName: name }, { quoted: m });
+
+
 await client.sendMessage(m.chat, {
-  video: {url: data.data.videoUrl},
-mimetype: "video/mp4",
- fileName: `${data.data.title}.mp4`}, { quoted: m });
-
-await client.sendMessage(m.chat, {
- document: {url: data.data.videoUrl},
-mimetype: "video/mp4",
- fileName: `${data.data.title}.mp4` }, { quoted: m });
+ document: {url: video},
+mimetype: "video/mp4", caption: name,
+ fileName: name }, { quoted: m });
 
 
-} catch (error) {
 
-m.reply("Download failed\n" + error)
 
-}
 
-}
+    } catch (error) {
+        m.reply("Download failed\n" + error.message);
+    }
+};
