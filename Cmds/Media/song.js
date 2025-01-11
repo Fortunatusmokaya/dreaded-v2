a8c7c7b8e72c4a296854fb2274a3d0d4d258afc9
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-11 09:46:57 +0300

    Update song.js

--- Cmds/Media/song.js
+++ Cmds/Media/song.js
@@ -1,33 +1,38 @@
-const axios = require("axios");
-
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
-
     const yts = require("yt-search");
 
     try {
         if (!text) return m.reply("What song do you want to download?");
-            const {
-                videos
-            } = await yts(text);
-            if (!videos || videos.length <= 0) {
-                m.reply(`No songs found!`)
-                return;
-            }
-            let urlYt = videos[0].url
 
+        const { videos } = await yts(text);
+        if (!videos || videos.length === 0) return m.reply("No songs found!");
+
+        const urlYt = videos[0].url;
         let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${urlYt}`);
-        let name = data.title;
-        let audio = data.audioUrl;
 
-        await m.reply(`_Downloading ${name}_`);
+        if (!data || !data.result || !data.result.download || !data.result.download.url) {
+            return m.reply("Failed to fetch audio from the API.");
+        }
+
+        const {
+            metadata: { title, thumbnail, duration, author },
+            download: { url: audioUrl, quality, filename },
+        } = data.result;
 
 
-        await client.sendMessage(m.chat, {
- audio: {url: audio},
-mimetype: "audio/mpeg",
- fileName: name }, { quoted: m });
 
+        await m.reply(`_Downloading ${title}_`);
+
+        await client.sendMessage(
+            m.chat,
+            {
+                audio: { url: audioUrl },
+                mimetype: "audio/mpeg",
+                fileName: filename,
+            },
+            { quoted: m }
+        );
     } catch (error) {
         m.reply("Download failed\n" + error.message);
     }
