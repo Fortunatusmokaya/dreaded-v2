5d41acfc9e2d54673ebf28aab3781f707c2c91f6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-28 14:09:04 +0300

    Update uptime.js

--- Cmds/General/uptime.js
+++ Cmds/General/uptime.js
@@ -1,20 +1,23 @@
-module.exports = {
-    name: "uptime",
-    aliases: ["runtime", "run"], 
-    run: async ({ m }) => {
-        const uptimes = function (seconds) {
-            seconds = Number(seconds);
-            var d = Math.floor(seconds / (3600 * 24));
-            var h = Math.floor((seconds % (3600 * 24)) / 3600);
-            var m = Math.floor((seconds % 3600) / 60);
-            var s = Math.floor(seconds % 60);
-            var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " Day, ") : "";
-            var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " Hours, ") : "";
-            var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " Minutes, ") : "";
-            var sDisplay = s > 0 ? s + (s == 1 ? " second" : " Seconds") : "";
-            return dDisplay + hDisplay + mDisplay + sDisplay;
-        };
+//uptime.js
 
-        await m.reply(`Uptime: ${uptimes(process.uptime())}`);
-    }
+module.exports = async (context) => {
+        const { m } = context;
+
+const uptimes = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " Day, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " Hours, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " Minutes, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " Seconds") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+
+
+await m.reply(`${uptimes(process.uptime())}`);
+
+
 };
