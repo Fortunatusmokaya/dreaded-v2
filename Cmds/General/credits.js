c731a0ea4b0b39749ab97e38816cfa472b38b55c
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-28 12:54:38 +0300

    Update credits.js

--- Cmds/General/credits.js
+++ Cmds/General/credits.js
@@ -1,14 +1,17 @@
 /* Why do you want to edit the credits ?, You may add yourself but do not OMIT any part */
 
 
+module.exports = {
+    name: "credits",
+   
+    run: async ({ client, m, prefix }) => {
 
-
-module.exports = async (context) => {
-    const { client, m, prefix } = context;
 
 
            await client.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/c75efecf7f0aef851fc02.jpg' }, caption: `We express sincere gratitude and acknowledgement to the following:\n\n -Dika Ardnt ➪ Indonesia\n - Writing the base code using case method\nhttps://github.com/DikaArdnt\n\n -Adiwajshing ➪ India\n - Writing and Coding the bot's library (baileys)\nhttps://github.com/WhiskeySockets/Baileys\n\n -WAWebSockets Discord Server community\n-Maintaining and reverse engineering the Web Sockets\nhttps://discord.gg/WeJM5FP9GG\n\n - Fortunatus Mokaya ➪ Kenya\n - Actively compiling and debugging parts of this bot script\nhttps://github.com/Fortunatusmokaya\n - Malik ➪ Kenya\n - Actively de-compiling, debugging and fixing parts of this bot script\nhttps://github.com/darkLo1rd\n\n - ChatGPT ➪ USA\n - Formulating ideas and assisting in debugging.\nhttps://chat.openai.com\n\n𝐷𝑟𝑒𝑎𝑑𝑒𝑑 𝐵𝑜𝑡 シ︎`}); 
 
+
+}
 
 }
 
