0e8358f6657db7aa4f333cb3a1228b9053c3ba02
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-28 13:11:56 +0300

    Update profilegc.js

--- Cmds/General/profilegc.js
+++ Cmds/General/profilegc.js
@@ -1,6 +1,7 @@
-module.exports = async (context) => {
-        const { client, m } = context;
-
+module.exports = {
+    name: "profilegc",
+    aliases: ["gcinfo"], 
+    run: async ({ client, m }) => {
 
 function convertTimestamp(timestamp) {
   const d = new Date(timestamp * 1000);
@@ -29,5 +30,7 @@ try {
 await client.sendMessage(m.chat, { image: { url: pp }, 
           caption: `_Name_ : *${info.subject}*\n\n_ID_ : *${info.id}*\n\n_Group owner_ : ${'@'+info.owner.split('@')[0]} || 'No Creator'\n\n_Group created_ : *${ts.day}, ${ts.date} ${ts.month} ${ts.year}, ${ts.time}*\n\n_Participants_ : *${info.size}*\n_Members_ : *${info.participants.filter((p) => p.admin == null).length}*\n\n_Admins_ : *${Number(info.participants.length - info.participants.filter((p) => p.admin == null).length)}*\n\n_Who can send message_ : *${info.announce == true ? 'Admins' : 'Everyone'}*\n\n_Who can edit group info_ : *${info.restrict == true ? 'Admins' : 'Everyone'}*\n\n_Who can add participants_ : *${info.memberAddMode == true ? 'Everyone' : 'Admins'}*`
         }, {quoted: m })
+
+}
 
 }
