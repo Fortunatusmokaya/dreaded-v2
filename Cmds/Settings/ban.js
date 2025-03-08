68750c5e7b34b33649a62ac578c25fb08a71c390
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 15:47:22 +0300

    Update ban.js

--- Cmds/Settings/ban.js
+++ Cmds/Settings/ban.js
@@ -1,5 +1,5 @@
 const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
-const { getSettings, banUser, getBannedUsers, getSudoUsers } = require('../../config');
+const { getSettings, banUser, getBannedUsers, getSudoUsers } = require('../../Database/config');
 
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
