a32c814f053b9c861d3d8624d075e84b006f8b49
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 19:39:38 +0300

    Update antilink.js

--- Functions/antilink.js
+++ Functions/antilink.js
@@ -1,7 +1,13 @@
-module.exports = async (client, m, isBotAdmin, isAdmin, Owner, body) => {
-    if (body && body.includes('chat.whatsapp.com') && !Owner && isBotAdmin && !isAdmin && m.isGroup) {
+const { getGroupSetting } = require("../Database/config");
 
-m.reply("Group link detected");
+module.exports = async (client, m, isBotAdmin, isAdmin, Owner, body) => {
+    if (!m.isGroup) return;
+
+    const antilink = await getGroupSetting(m.chat, "antilink");
+
+    if (antilink === true && body.includes("chat.whatsapp.com") && !Owner && isBotAdmin && !isAdmin) {
+        m.reply("Group link detected");
+
         const kid = m.sender;
         await client.sendMessage(m.chat, {
             delete: {
@@ -11,9 +17,11 @@ m.reply("Group link detected");
                 participant: kid
             }
         });
-        await client.groupParticipantsUpdate(m.chat, [kid], 'remove');
+
+        await client.groupParticipantsUpdate(m.chat, [kid], "remove");
+
         await client.sendMessage(m.chat, {
-            text: `Removed!\n\n@${kid.split("@")[0]} sending group links is prohibited!`,
+            text: `Removed!\n\n@${kid.split("@")[0]}, sending group links is prohibited!`,
             contextInfo: {
                 mentionedJid: [kid]
             }
