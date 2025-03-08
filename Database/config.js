de3ca7772da8bf9a5be7e20d2d5a2e19685e2cdc
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 20:32:37 +0300

    Update config.js

--- Database/config.js
+++ Database/config.js
@@ -85,7 +85,7 @@ const defaultGroupSettings = {
     antidemote: 'false',      
     antipromote: 'true',
     events: 'true',
-    antilink: 'on'
+    antilink: 'true'
 };
 
 async function initializeGroupSettings(jid) {
