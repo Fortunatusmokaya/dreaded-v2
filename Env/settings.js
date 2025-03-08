bc286e5a0bf94cdaac0895b2a3306f24281e1cea
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 16:38:10 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,1 +1,9 @@
- 
+ const session = process.env.SESSION || '';
+const mycode = process.env.CODE || "254";
+const botname = process.env.BOTNAME || 'DREADED';
+
+module.exports = {
+  session,
+  mycode,
+  botname
+};
