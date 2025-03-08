af508a37959abfc498e6aa0956c653b8999a9454
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 21:47:37 +0300

    Update blocked_users.js

--- Functions/blocked_users.js
+++ Functions/blocked_users.js
@@ -1,4 +1,6 @@
-module.exports = async (client, m, cmd) => {
+/* module.exports = async (client, m, cmd) => {
     const Blocked = await client.fetchBlocklist();
     return cmd && m.isGroup && Blocked.includes(m.sender);
 };
+
+*/
