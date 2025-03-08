cbd4d8accba8c92b7b667e3ccafd9714827dabff
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 19:10:18 +0300

    Delete settings.js

--- settings.js
+++
@@ -1,9 +0,0 @@
-const session = process.env.SESSION || '';
-const mycode = process.env.CODE || "254";
-const botname = process.env.BOTNAME || 'DREADED';
-
-module.exports = {
-  session,
-  mycode,
-  botname
-};
