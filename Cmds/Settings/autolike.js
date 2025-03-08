6f5c661c9d4926082bb8d080c07c029917d6a3da
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 04:14:22 +0300

    Update autolike.js

--- Cmds/Settings/autolike.js
+++ Cmds/Settings/autolike.js
@@ -1,5 +1,4 @@
-const { updateSetting } = require('../../config');
-const { settings } = require('../../loadSettings');
+const { getSettings, updateSetting } = require('../../config');
 const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
 
 module.exports = async (context) => {
@@ -7,22 +6,22 @@ module.exports = async (context) => {
         const { m, args } = context;
         const value = args[0]?.toLowerCase();
 
+        const settings = await getSettings();
+
         if (value === 'on') {
             if (settings.autolike) {
                 return await m.reply('✅ Autolike is already ON.');
             }
             await updateSetting('autolike', 'true');
-          
             await m.reply('✅ Autolike has been turned ON. The bot will now like status updates.');
         } else if (value === 'off') {
             if (!settings.autolike) {
                 return await m.reply('❌ Autolike is already OFF.');
             }
             await updateSetting('autolike', 'false');
-            
             await m.reply('❌ Autolike has been turned OFF.');
         } else {
-            await m.reply(`📄 Current autolike setting: ${settings.autolike ? 'ON' : 'OFF'}\n\nUse _${settings.prefix}autolike on_ or _${settings.prefix}autolike off_._`);
+            await m.reply(`📄 Current autolike setting: ${settings.autolike ? 'ON' : 'OFF'}\n\nUse _${settings.prefix}autolike on_ or _${settings.prefix}autolike off_.`);
         }
     });
 };
