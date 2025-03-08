4081fce65c336ce25601203addf883cf6a2d6789
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-08 16:37:52 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,50 +1,9 @@
-/* Bot settings 
-
-You don't have to set this if you deploy using heroku because you can simply set them in environment variables, also don't forget to sleep */
-
-
 const session = process.env.SESSION || '';
-
-const prefix = process.env.PREFIX || '.';
 const mycode = process.env.CODE || "254";
-const author = process.env.STICKER_AUTHOR || 'fortunatus';
-const packname = process.env.PACKNAME || 'dreaded md2 🤖';
-const dev = process.env.DEV || '254114018035';
-const DevDreaded = dev.split(",");
 const botname = process.env.BOTNAME || 'DREADED';
-const mode = process.env.MODE || 'public';
-const gcpresence = process.env.GC_PRESENCE || 'false';
-const antionce = process.env.ANTIVIEWONCE || 'true';
-const sessionName = "session";
-const cookies = JSON.parse(process.env.COOKIE || '[{"domain":".youtube.com","expirationDate":1764463277.409877,"hostOnly":false,"httpOnly":false,"name":"__Secure-1PAPISID","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"UoBcKfo0_FSAxQ5D/A5ZClpB2xVLQJQGUx","id":1},{"domain":".youtube.com","expirationDate":1764463277.412158,"hostOnly":false,"httpOnly":true,"name":"__Secure-1PSID","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"g.a000pghxevPjwTr5Un_D-PS1UxiaEdymANhc_5NWNQgaApthzLU0MOFGGamQ5yqi2vrAqKldbgACgYKASoSARUSFQHGX2MiB0PtUQYJy2_oQLkmMPXgfRoVAUF8yKpuqWya_M2xRHe_6e9o_6TK0076","id":2},{"domain":".youtube.com","expirationDate":1762941611.655441,"hostOnly":false,"httpOnly":true,"name":"__Secure-1PSIDCC","path":"/","sameSite":"unspecified","secure":true,"session":false,"storeId":"0","value":"AKEyXzWtrmvqerXnEweUSkGiFKAn57TBnvoAEBDi6B33Sg4gpMOANgVFwDBU_JtKQXLpisy_","id":3}]');
-const presence = process.env.WA_PRESENCE || 'online';
-
-const antitag = process.env.ANTITAG || 'true';
-const antidelete = process.env.ANTIDELETE || 'true';
-const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
-const autoread = process.env.AUTOREAD || 'true';
-const autobio = process.env.AUTOBIO || 'false';
 
 module.exports = {
-  sessionName,
-  presence,
-  autoview,
-  autoread,
-  botname,
-  cookies,
-  autobio,
-  mode,
-autolike,
-  prefix,
+  session,
   mycode,
-  author,
-  packname,
-  dev,
-  DevDreaded,
-  gcpresence,
-  antionce,
-session,
-antitag,
-antidelete
+  botname
 };
