a004e3963c3bfeb978a8de819d42dda152b10b58
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 11:54:33 +0300

    Update Wa-Privacy

---
+++ Cmds/Wa-Privacy/groupadd.js
@@ -0,0 +1,24 @@
+module.exports = async (context) => {
+
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
+
+    await ownerMiddleware(context, async () => {
+
+    const { client, m, text} = context;
+
+if (!text) {
+      m.reply("Provide a setting to be updated. Example:\ngroupadd all");
+      return;
+    }
+
+
+const availablepriv = ['all', 'contacts', 'contact_blacklist', 'none'];
+
+if (!availablepriv.includes(text)) return m.reply(`Choose a setting from this list: ${availablepriv.join('/')}`);
+
+await client.updateGroupsAddPrivacy(text)
+        await m.reply(`Group Add privacy settings updated to *${text}*`);
+
+})
+
+}
