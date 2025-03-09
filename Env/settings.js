357ac156deea50b6fb74566e5235d3c5fbc79ecb
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 21:34:25 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -3,11 +3,13 @@ const mycode = process.env.CODE || "254";
 const botname = process.env.BOTNAME || 'DREADED';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
+const database = process.env.DATABASE_URL || '';
 
 module.exports = {
   session,
   mycode,
   botname,
+  database,
 herokuAppName,
 herokuApiKey
 };
