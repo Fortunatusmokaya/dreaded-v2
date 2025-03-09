d9d784169bde1bf837c85fd6a01bf37b998e0f1d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 11:52:34 +0300

    Update Wa-Privacy

---
+++ Cmds/Wa-Privacy/mypp.js
@@ -0,0 +1,23 @@
+module.exports = async (context) => {
+
+const ownerMiddleware = require('../../utility/botUtil/Ownermiddleware');
+    await ownerMiddleware(context, async () => {
+
+    const { client, m, text} = context;
+
+if (!text) {
+      m.reply("Provide a setting to be updated. Example:\nmydp all");
+      return;
+    }
+
+
+const availablepriv = ['all', 'contacts', 'contact_blacklist', 'none'];
+
+if (!availablepriv.includes(text)) return m.reply(`Choose a setting from this list: ${availablepriv.join('/')}`);
+
+await client.updateProfilePicturePrivacy(text)
+        await m.reply(`Profile Picture privacy settings updated to *${text}*`);
+
+})
+
+}
