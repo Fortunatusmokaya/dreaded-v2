2c90b8847cc25aa963a7dd3ed462ac0392c9f83e
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 11:49:50 +0300

    Update Wa-Privacy

---
+++ Cmds/Wa-Privacy/privacy.js
@@ -0,0 +1,40 @@
+module.exports = async (context) => {
+
+const ownerMiddleware = require('../../Middleware/ownerMiddleware');
+    await ownerMiddleware(context, async () => {
+
+    const { client, m } = context;
+
+const Myself = await client.decodeJid(client.user.id);
+    
+    const {
+                readreceipts,
+                profile,
+                status,
+                online,
+                last,
+                groupadd,
+                calladd
+        } = await client.fetchPrivacySettings(true);
+        
+        const fnn = `*Current Privacy Settings*
+
+* Name : ${client.user.name}
+* Online: ${online}
+* Profile picture : ${profile}
+* Last seen : ${last}
+* Read receipt : ${readreceipts}
+* Group add : ${groupadd}
+* Status : ${status}
+* Call add : ${calladd}`;
+
+
+const avatar = await client.profilePictureUrl(Myself, 'image').catch(_ => 'https://telegra.ph/file/b34645ca1e3a34f1b3978.jpg');
+
+await client.sendMessage(m.chat, { image: { url: avatar}, caption: fnn}, { quoted: m}) 
+
+
+})
+
+}
+        
