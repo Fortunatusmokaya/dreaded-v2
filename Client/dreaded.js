ce4a68c8ff5b6bc6efe700cf70352d78cc53f760
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 12:24:31 +0300

    Update dreaded.js

--- Client/dreaded.js
+++ Client/dreaded.js
@@ -7,69 +7,66 @@ const { smsg, formatp, tanggal, formatDate, getTime, sleep, clockString, fetchJs
 const { exec, spawn, execSync } = require("child_process");
 const { TelegraPh, UploadFileUgu } = require("../lib/toUrl");
 const uploadtoimgur = require('../lib/Imgur');
-const { readFileSync } = require('fs'); 
 
 const { commands, aliases, totalCommands } = require('../Handler/commandHandler');
-// const blocked_users = require('../Functions/blocked_users');
 const status_saver = require('../Functions/status_saver');
 const gcPresence = require('../Functions/gcPresence');
-// const antilinkgc = require('../Functions/antilink');
 const antitaggc = require('../Functions/antitag');
 const antidel = require('../Functions/antidelete');
 
-const { getSettings, getSudoUsers } = require('../Database/config');
-
-const {
-    botname, mycode } = require('../Env/settings');
+const { getSettings, getSudoUsers, getBannedUsers } = require('../Database/config');
+const { botname, mycode } = require('../Env/settings');
 
 module.exports = dreaded = async (client, m, chatUpdate, store) => {
-
-try {
-
-    const sudoUsers = await getSudoUsers();
-
-let settings = await getSettings();
+    try {
+        const sudoUsers = await getSudoUsers();
+        const bannedUsers = await getBannedUsers();
+        let settings = await getSettings();
         if (!settings) return;
 
         const { prefix, mode, gcpresence, antitag, antidelete, antilink, packname } = settings;
 
-
         var body =
-            m.mtype === "conversation"
-                ? m.message.conversation
-                : m.mtype === "imageMessage"
-                    ? m.message.imageMessage.caption
-                    : m.mtype === "extendedTextMessage"
-                        ? m.message.extendedTextMessage.text
-                        : "";
+            m.mtype === "conversation" ? m.message.conversation :
+            m.mtype === "imageMessage" ? m.message.imageMessage.caption :
+            m.mtype === "extendedTextMessage" ? m.message.extendedTextMessage.text : "";
 
-        const Tag =
-            m.mtype == "extendedTextMessage" &&
-            m.message.extendedTextMessage.contextInfo != null
-                ? m.message.extendedTextMessage.contextInfo.mentionedJid
-                : [];
+        const args = body.trim().split(/\s+/);
+        const commandName = args.length > 0 ? args[0].toLowerCase().replace(prefix, "") : null;
+        const resolvedCommandName = aliases[commandName] || commandName;
 
-        var msgDreaded = m.message.extendedTextMessage?.contextInfo?.quotedMessage;
-        var budy = typeof m.text == "string" ? m.text : "";
+        const isCommand = commands[resolvedCommandName] || body.startsWith(prefix);
+       // if (!isCommand) return;
+
+        const pushname = m.pushName || "No Name";
+        const botNumber = await client.decodeJid(client.user.id);
+        const itsMe = m.sender == botNumber;
+        const Owner = sudoUsers.includes(m.sender.replace(/[^0-9]/g, "") + "@s.whatsapp.net");
+
+       
+        if (isCommand && bannedUsers.includes(m.sender)) {
+            await client.sendMessage(m.chat, { text: "You are banned from using bot commands." }, { quoted: m });
+            return;
+        }
+
+        
+        if (isCommand && mode === "private" && !itsMe && !Owner) {
+            return;
+        }
 
         const timestamp = speed();
         const dreadedspeed = speed() - timestamp;
 
-const path = require('path');
+        const path = require('path');
+        const filePath = path.resolve(__dirname, '../dreaded.jpg'); 
+        const pict = fs.readFileSync(filePath);
 
-const filePath = path.resolve(__dirname, '../dreaded.jpg'); 
-const pict = fs.readFileSync(filePath);
+        const Tag = m.mtype == "extendedTextMessage" && m.message.extendedTextMessage.contextInfo != null
+            ? m.message.extendedTextMessage.contextInfo.mentionedJid
+            : [];
 
-
-
-        const cmd = body.startsWith(prefix);
-        const args = body.trim().split(/ +/).slice(1);
-        const pushname = m.pushName || "No Name";
-        const botNumber = await client.decodeJid(client.user.id);
-        const itsMe = m.sender == botNumber ? true : false;
-        let text = (q = args.join(" "));
-        const arg = budy.trim().substring(budy.indexOf(" ") + 1);
-        const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+        const msgDreaded = m.message.extendedTextMessage?.contextInfo?.quotedMessage;
+        const budy = typeof m.text == "string" ? m.text : "";
 
         const getGroupAdmins = (participants) => {
             let admins = [];
@@ -78,20 +75,14 @@ const pict = fs.readFileSync(filePath);
             }
             return admins || [];
         };
-        const fortu = (m.quoted || m);
-        const quoted = (fortu.mtype == 'buttonsMessage') ? fortu[Object.keys(fortu)[1]] : (fortu.mtype == 'templateMessage') ? fortu.hydratedTemplate[Object.keys(fortu.hydratedTemplate)[1]] : (fortu.mtype == 'product') ? fortu[Object.keys(fortu)[0]] : m.quoted ? m.quoted : m;
 
-        const color = (text, color) => {
-            return !color ? chalk.green(text) : chalk.keyword(color)(text);
-        };
+        const fortu = (m.quoted || m);
+        const quoted = (fortu.mtype == 'buttonsMessage') ? fortu[Object.keys(fortu)[1]] :
+            (fortu.mtype == 'templateMessage') ? fortu.hydratedTemplate[Object.keys(fortu.hydratedTemplate)[1]] :
+            (fortu.mtype == 'product') ? fortu[Object.keys(fortu)[0]] : m.quoted ? m.quoted : m;
+
         const mime = (quoted.msg || quoted).mimetype || "";
         const qmsg = (quoted.msg || quoted);
-
-
-
-
-        const DevDreaded = Array.isArray(sudoUsers) ? sudoUsers : [];
-        const Owner = DevDreaded.map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m.sender);
 
         const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => { }) : "";
         const groupName = m.isGroup && groupMetadata ? await groupMetadata.subject : "";
@@ -102,35 +93,18 @@ const pict = fs.readFileSync(filePath);
         const IsGroup = m.chat?.endsWith("@g.us");
 
         const context = {
-            client, m, text, Owner, chatUpdate, store, isBotAdmin, isAdmin, IsGroup, participants,
+            client, m, Owner, chatUpdate, store, isBotAdmin, isAdmin, IsGroup, participants,
             pushname, body, budy, totalCommands, args, mime, qmsg, msgDreaded, botNumber, itsMe,
             packname, generateProfilePicture, groupMetadata, dreadedspeed, mycode,
-            fetchJson, exec, getRandom, UploadFileUgu, TelegraPh, prefix, cmd, botname, mode, gcpresence, antitag, antidelete, fetchBuffer, store, uploadtoimgur, chatUpdate, getGroupAdmins, pict, Tag
+            fetchJson, exec, getRandom, UploadFileUgu, TelegraPh, prefix, botname, mode, gcpresence, antitag, antidelete, fetchBuffer, store, uploadtoimgur, chatUpdate, getGroupAdmins, pict, Tag
         };
 
-        if (cmd && mode === 'private' && !itsMe && !Owner && m.sender !== sudoUsers) {
-            return;
-        }
-
-       /* if (await blocked_users(client, m, cmd)) {
-            await m.reply("You are blocked from using bot commands.");
-            return;
-        }
-
-*/
-
+      
         await antidel(client, m);
         await status_saver(client, m, Owner, prefix);
-    
         // await antilinkgc(client, m, isBotAdmin, isAdmin, Owner, body);
-      
         await gcPresence(client, m);
         await antitaggc(client, m, isBotAdmin, itsMe, isAdmin, Owner, body);
-    
-
-
-        const commandName = body.startsWith(prefix) ? body.slice(prefix.length).trim().split(/\s+/)[0].toLowerCase() : null;
-        const resolvedCommandName = aliases[commandName] || commandName;
 
         if (commands[resolvedCommandName]) {
             await commands[resolvedCommandName](context);
