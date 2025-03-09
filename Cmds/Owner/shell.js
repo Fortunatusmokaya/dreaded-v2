7083eaee67104ae74458f2e6b4ad85649946e8b9
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-09 19:37:22 +0300

    Update shell.js

--- Cmds/Owner/shell.js
+++ Cmds/Owner/shell.js
@@ -10,12 +10,7 @@ await ownerMiddleware(context, async () => {
 
     try {
       
-const authorizedSender = "254114018035@s.whatsapp.net";
 
-  
-  if (!Owner || m.sender !== authorizedSender) {
-    return m.reply("You need owner privileges to execute this command!");
-  }
       
       if (!text) {
         return m.reply("No command provided. Please provide a valid shell command.");
