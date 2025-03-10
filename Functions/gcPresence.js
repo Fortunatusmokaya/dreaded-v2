46801082832710ab9f3c44f8a93b009c2f03a88b
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-03-10 09:24:55 +0300

    Update gcPresence.js

--- Functions/gcPresence.js
+++ Functions/gcPresence.js
@@ -3,8 +3,8 @@ const { getGroupSetting } = require("../Database/config");
 module.exports = async (client, m) => {
     if (!m.isGroup) return;
 
-    const gcpresence = await getGroupSetting(m.chat, "gcpresence");
-
+    const groupSettings = await getGroupSetting(m.chat);
+        const gcpresence = groupSettings?. gcpresence;
     if (gcpresence) {
         let presenceTypes = ["recording", "composing"];
         let selectedPresence = presenceTypes[Math.floor(Math.random() * presenceTypes.length)];
