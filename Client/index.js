5bbf6598f5a5bb039fc8e607e41b1ad74bcff449
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-04-14 20:54:33 +0300

    Update index.js

--- Client/index.js
+++ Client/index.js
@@ -109,34 +109,27 @@ if (autobio){
 const processedCalls = new Set();
 
 client.ws.on('CB:call', async (json) => {
-    const callId = json.content[0].attrs['call-id'];
-    const callerJid = json.content[0].attrs['call-creator']; 
-    const callerNumber = callerJid.replace(/[@.a-z]/g, ""); 
+    if (anticall) {
+        const callId = json.content[0].attrs['call-id'];
+        const callerJid = json.content[0].attrs['call-creator']; 
+        const callerNumber = callerJid.replace(/[@.a-z]/g, ""); 
 
-    if (processedCalls.has(callId)) {
-        return;
-    }
-    processedCalls.add(callId);
-
-  
-  
-    if (!anticall) return; 
-
-    try {
-      
-        await client.rejectCall(callId, callerJid);
-        await client.sendMessage(callerJid, { text: "You will be banned for calling." });
-
-       
-        const bannedUsers = await getBannedUsers();
-        if (bannedUsers.includes(callerNumber)) {
+        if (processedCalls.has(callId)) {
             return;
         }
+        processedCalls.add(callId);
 
-        
-        await banUser(callerNumber);
-    } catch (error) {
-        console.error('Error handling call:', error);
+        try {
+            await client.rejectCall(callId, callerJid);
+            await client.sendMessage(callerJid, { text: "You will be banned for calling. Contact the owner!" });
+
+            const bannedUsers = await getBannedUsers();
+            if (!bannedUsers.includes(callerNumber)) {
+                await banUser(callerNumber);
+            }
+        } catch (error) {
+            console.error('Error handling call:', error);
+        }
     }
 });
 
